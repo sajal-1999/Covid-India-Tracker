2964e05bade057544aa4c7336da03d2236f263bc
arunimaagl <arunima4499@gmail.com>
2020-07-26 22:49:11 +0530

    ..

--- app.py
+++ app.py
@@ -2,16 +2,30 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
+# import plotly.express as px
+import plotly.graph_objects as go
 from get_data import get_data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-fig = px.line(get_data(), x = 'Date', y=['Total Confirmed', 'Total Recovered', 'Total Deceased'])
+# fig = px.line(get_data(), x = 'Date', y=['Total Confirmed', 'Total Recovered', 'Total Deceased'])
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Confirmed'], name='Total Cofirmed', line=dict(color='blue')))
+fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Deceased'], name='Total Deceased', line=dict(color='red', dash='dot')))
+fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Recovered'], name='Total Recovered', line=dict(color='green')))
+# fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Confirmed'], line=dict(color='blue')))
 
-app.layout = html.Div(children=[
+fig.update_layout(
+    xaxis_title = 'Date',
+    yaxis_title = 'Cases',
+    title = 'India overall cases'
+#     plot_bgcolor="black"
+)
+
+app.layout = html.Div(#style={'backgroundColor': 'black'},
+    children=[
     html.H1('Hello user', style = {'textAlign': 'center', 'color': 'red'}),
     html.Div(children = '''
         Dash: A web application framework for Python.
