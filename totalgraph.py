e5d9ce8efc349cfd654820ab899241a86060aab6
arunimaagl <arunima4499@gmail.com>
2020-07-27 18:52:04 +0530

    update graph

--- totalgraph.py
+++ totalgraph.py
@@ -3,25 +3,40 @@ import dash_core_components as dcc
 import plotly.graph_objs as go
 from get_data import get_data
 
-# fig = px.line(get_data(), x = 'Date', y=['Total Confirmed', 'Total Recovered', 'Total Deceased'])
-# fig = go.Figure()
-# fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Confirmed'], name='Total Cofirmed', line=dict(color='blue')))
-# fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Deceased'], name='Total Deceased', line=dict(color='red', dash='dot')))
-# fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Recovered'], name='Total Recovered', line=dict(color='green')))
-# fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Active'], name='Total Active', line=dict(color='yellow')))
-# fig.add_trace(go.Scatter(x=get_data()['Date'], y=get_data()['Total Confirmed'], line=dict(color='blue')))
-
-confirmed = go.Scatter(x=get_data()['Date'], y=get_data()['Total Confirmed'], name='Total Cofirmed', line=dict(color='#3498DB'))
-active = go.Scatter(x=get_data()['Date'], y=get_data()['Active'], name='Total Active', line=dict(color='#F39C12'))
-recovered = go.Scatter(x=get_data()['Date'], y=get_data()['Total Recovered'], name='Total Recovered', line=dict(color='#00bc8c'))
-deceased = go.Scatter(x=get_data()['Date'], y=get_data()['Total Deceased'], name='Total Deceased', line=dict(color='#E74C3C', dash='dot'))
+confirmed = go.Scatter(
+    x=get_data()['Date'], 
+    y=get_data()['Total Confirmed'], 
+    name='Total Cofirmed', 
+    hovertemplate = 'Confirmed: %{y}<extra></extra>',
+    line=dict(color='#3498DB')
+)
+active = go.Scatter(
+    x=get_data()['Date'], 
+    y=get_data()['Active'], 
+    name='Total Active', 
+    hovertemplate = 'Active: %{y}<extra></extra>',
+    line=dict(color='#F39C12')
+)
+recovered = go.Scatter(
+    x=get_data()['Date'], 
+    y=get_data()['Total Recovered'], 
+    name='Total Recovered',
+    hovertemplate = 'Recovered: %{y}<extra></extra>',
+    line=dict(color='#00bc8c')
+)
+deceased = go.Scatter(
+    x=get_data()['Date'], 
+    y=get_data()['Total Deceased'], 
+    name='Total Deceased',
+    hovertemplate = 'Deceased: %{y}<extra></extra>',
+    line=dict(color='#E74C3C')
+)
 
 fig = go.Figure(
     data = [confirmed, active, recovered, deceased],
     layout = go.Layout(
         legend = dict(font=dict(color="#FFFFFF")),
         title = 'India overall cases',
-        hovermode="x",
         xaxis = dict(title = 'Date', 
                     color="#e8eaed", 
                     dtick=10,
@@ -34,6 +49,9 @@ fig = go.Figure(
                     tickfont=dict(family="Times New Roman", color = "#e8eaed")),
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(0,0,0,0)',
+        dragmode=False,
+        hovermode="x",
+        # hoverlabel=dict(bgcolor="#4d4e50"),
         titlefont=dict(color='#FFFFFF')
     )
 )
