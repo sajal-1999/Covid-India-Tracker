52c8880d00689921e22b4f49505a3d67131a40ae
arunimaagl <arunima4499@gmail.com>
2020-07-28 23:33:36 +0530

    update readme

--- README.md
+++ README.md
@@ -3,26 +3,9 @@
 ## A detailed graphical depiction of India's current Covid-19 stats.
 ---
 ### to-do list
-- [x] add a "active" column to the data
-- [x] add a total stats display at top
-- [x] remove plotly navigate graph menu
-- [x] set graph backgrounds to dark mode
-- [x] function to return state_to_district mapping
-- [x] function to return state_data
-- [x] function to return district_data
-- [x] change layout of India Graph
-- [x] change textfield to drop-down menu in navbar
-- [x] initial callback for district dropdown menu
-- [x] add second graph based on state selected
-- [x] decide upon dropdown menu displays (dbc vs dcc)
-- [x] add callback for navbar dropdown menus
-- [x] resize graph
-- [x] set initial 2nd graph display
-- [x] modify 2nd graph to display update_trace funtion
-- [x] optimize codes for 2nd graph
-- [x] add callback for selected district
-- [ ] add card list to show the selected state's stats alongside the district
+- [ ] fix card layout
+- [ ] fix graph width
 - [ ] add links to github/data source/gmail at the bottom
-- [ ] add padding to total stats display and center it
 - [ ] fix the navbar toast
+- [ ] fix navbar display
 - [ ] integrate mailing list
--- select_graph_att.py
+++ select_graph_att.py
@@ -1,47 +1,17 @@
 from get_data import get_state_list
-import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 
-state_dcc = dcc.Dropdown(
-        id="state-selected-dcc",
+state = dcc.Dropdown(
+        id="state-selected",
         value='Delhi',
         clearable=False,
         style=dict(width = '250px', color="black", verticalAlign="right"),
         options=[{'label':state_name, 'value':state_name} for state_name in get_state_list()]
     )
 
-district_dcc = dcc.Dropdown(
-        id="district-selected-dcc",
+district = dcc.Dropdown(
+        id="district-selected",
         clearable=False,
         style=dict(width = '250px', color="black", marginLeft="25px", verticalAlign="middle"),
         # options=[{'label':district_name, 'value':district_name} for district_name in get_state_to_district_mapping(state_name)]
     )
-
-
-# state_list = []
-# for i in get_state_list():
-#     state_list.append(dbc.DropdownMenuItem(i, id=i))
-
-# district_list = []
-# state_name = 'Maharashtra' #Kept static for now. Need to fetch from state drop-down
-# for i in get_state_to_district_mapping(state_name):
-#     district_list.append(dbc.DropdownMenuItem(i, id= i))
-
-
-# state = dbc.InputGroup([
-#     dbc.DropdownMenu(
-#         label="Select State",
-#         id="state-selected",
-#         children=state_list,
-#         className="mb-3",
-#         right=True
-# )])
-
-# district = dbc.InputGroup([
-#     dbc.DropdownMenu(
-#         label="Select District",
-#         id="district-selected",
-#         children=district_list,
-#         className="mb-3",
-#         right=True
-# )])
